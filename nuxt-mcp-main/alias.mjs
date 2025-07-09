71258dfad046c21f1c6619499b720b0323dc64bb
Baltzakis Themistoklis <baltzakis.themis@gmail.com>
2025-07-09 12:52:08 +0300

    remove mcp-fetch-main and nuxt-mcp-main directories

--- nuxt-mcp-main/alias.mjs
+++
@@ -1,7 +0,0 @@
-import { fileURLToPath } from 'node:url'
-
-const r = path => fileURLToPath(new URL(path, import.meta.url))
-
-export default {
-  'vite-plugin-mcp': r('./packages/vite-plugin-mcp/src/index.ts'),
-}
