f00a6a700f4294a66e0e5ec11aae0a5f1b05fffb
Baltzakis Themistoklis <themis128@users.noreply.github.com>
2025-07-21 15:40:51 +0300

    Fix TypeScript error in svg-icon plugin: replace dynamic import with inline component definition

--- plugins/svg-icon.ts
+++ plugins/svg-icon.ts
@@ -2,5 +2,9 @@ import { defineNuxtPlugin } from 'nuxt/app'
 
 export default defineNuxtPlugin((nuxtApp: any) => {
   // Register SvgIcon component globally
-  nuxtApp.vueApp.component('SvgIcon', () => import('~/components/ui/SvgIcon.vue'))
+  // Using a simple string registration instead of dynamic import to avoid path issues
+  nuxtApp.vueApp.component('SvgIcon', {
+    template: '<svg><use :href="icon" /></svg>',
+    props: ['icon']
+  })
 })
