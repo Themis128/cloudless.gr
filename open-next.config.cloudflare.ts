aa61b891251e26bb048b903099844b8b6577e2cb
Themis Baltzakis <tbaltzakis@cloudless.gr>
2026-08-08 07:28:15 +0300

    ok2

--- open-next.config.cloudflare.ts
+++ open-next.config.cloudflare.ts
@@ -1,41 +1,16 @@
 /**
  * OpenNext.js Cloudflare configuration for cloudless.gr
- * Manually constructed to satisfy the validator exactly.
+ * Uses defineCloudflareConfig from the package to properly configure all overrides.
  */
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 import d1TagCache from "@opennextjs/cloudflare/overrides/tag-cache/d1-next-tag-cache";
 import { MemoryQueue } from "@opennextjs/cloudflare/overrides/queue/memory-queue";
-import assetResolver from "@opennextjs/cloudflare/overrides/asset-resolver";
 
-export default {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: d1TagCache,
-      queue: new MemoryQueue(),
-      cdnInvalidation: "dummy",
-    },
-    routePreloadingBehavior: "none",
-  },
-  edgeExternals: ["node:crypto"],
-  cloudflare: {
-    useWorkerdCondition: true,
-  },
-  dangerous: {
-    enableCacheInterception: false,
-  },
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-    assetResolver: () => assetResolver,
-  },
-};
+export default defineCloudflareConfig({
+  incrementalCache: "dummy",
+  tagCache: d1TagCache,
+  queue: new MemoryQueue(),
+  cachePurge: "dummy",
+  routePreloadingBehavior: "none",
+  enableCacheInterception: false,
+});
