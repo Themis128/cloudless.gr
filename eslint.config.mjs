21253e36338fa7b11f76d3a7a644be523ca0b7dc
Themis Baltzakis <tbaltzakis@cloudless.gr>
2026-08-17 20:18:33 +0300

    commit again 5

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextConfig from "eslint-config-next/index.js";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  nextConfig,
 
   // Project-specific rules
   {
